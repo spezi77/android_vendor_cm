b1b192306c9baa886839dd15c1aa04d896573800
spezi77 <spezi7713@gmail.com>
2015-02-01 14:20:38 +0100

    Disable ADB authentication
    Change-Id: I25c29620f0beb9f6ff50251dc4c13b6d2b86bde3

--- config/common.mk
+++ config/common.mk
@@ -67,8 +67,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
-# Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+# Disable ADB authentication
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 endif
 
 # Copy over the changelog to the device
