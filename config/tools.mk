1e07185f805189740f50885f41452b475cdcad06
spezi77 <spezi77@gmx.com>
2015-04-19 13:08:20 +0200

    Housekeeping * Don't copy A2SD scripts * Don't use prebuilt LatinIME (we use now the one by CM) * Update Terminal Emulator and NovaLauncher

--- config/tools.mk
+++ config/tools.mk
@@ -2,17 +2,17 @@ LOCAL_PATH:= vendor/cm/prebuilt/common
 PROP_PATH:= vendor/cm/proprietary
 
 # Utilize init.d scripts
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/etc/init.d/05mountext:system/etc/init.d/05mountext \
-    $(LOCAL_PATH)/etc/init.d/10apps2sd-redux:system/etc/init.d/10apps2sd \
-    $(LOCAL_PATH)/etc/init.d/20extgapps:system/etc/init.d/20extgapps
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/etc/init.d/05mountext:system/etc/init.d/05mountext \
+#    $(LOCAL_PATH)/etc/init.d/10apps2sd-redux:system/etc/init.d/10apps2sd \
+#    $(LOCAL_PATH)/etc/init.d/20extgapps:system/etc/init.d/20extgapps
 
 # Apps2sd files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bin/a2sd:system/bin/a2sd \
-    $(LOCAL_PATH)/bin/sysinit:system/bin/sysinit \
-    $(LOCAL_PATH)/bin/fix_permissions:system/bin/fix_permissions \
-    $(LOCAL_PATH)/xbin/zipalign:system/xbin/zipalign
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/bin/a2sd:system/bin/a2sd \
+#    $(LOCAL_PATH)/bin/sysinit:system/bin/sysinit \
+#    $(LOCAL_PATH)/bin/fix_permissions:system/bin/fix_permissions \
+#    $(LOCAL_PATH)/xbin/zipalign:system/xbin/zipalign
 
 # Compcache/Zram support
 PRODUCT_COPY_FILES += \
@@ -25,13 +25,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/lib/libjni_unbundled_latinimegoogle.so:system/lib/libjni_unbundled_latinimegoogle.so
 
 # Slim's prebuilt Latinime
-PRODUCT_COPY_FILES += \
-    $(PROP_PATH)/LatinIME.apk:system/app/LatinIME.apk
-
-# Terminal Emulator
-#PRODUCT_COPY_FILES +=  \
-#    $(PROP_PATH)/Term.apk:system/app/Term.apk \
-#    $(PROP_PATH)/lib/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+#PRODUCT_COPY_FILES += \
+#    $(PROP_PATH)/LatinIME.apk:system/app/LatinIME.apk
     
 # Copy NovaLauncher
 PRODUCT_COPY_FILES += \
