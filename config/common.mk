3c458907e661f305201d818277efa170347768c3
spezi77 <spezi77@gmx.com>
2015-09-15 23:25:19 +0200

    Add support for picking halfres bootanimation
    Change-Id: I46c4f0122180450e4696ab948dc05c859f11e5c4

--- config/common.mk
+++ config/common.mk
@@ -29,7 +29,11 @@ $(eval TARGET_BOOTANIMATION_NAME := $(shell \
 endef
 $(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
 
-PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
+ifeq ($(TARGET_BOOTANIMATION_HALF_RES),true)
+PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/halfres/$(TARGET_BOOTANIMATION_NAME).zip
+else
+PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip 
+endif
 endif
 
 ifdef CM_NIGHTLY
@@ -217,6 +221,7 @@ PRODUCT_COPY_FILES +=  \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
+
 else
 
 PRODUCT_PROPERTY_OVERRIDES += \
